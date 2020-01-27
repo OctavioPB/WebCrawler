568806432bffe6322469427a8df6b92493e4a43c
tavor <tavoriddler@gmail.com>
2020-01-26 17:15:09 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+''' This program crawls wikipedia and obtains the title, the first paragraph, and navigate to another article'''
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen('https://en.wikipedia.org/wiki/Combinatorics'.format(pageUrl))  # Read the wikipedia article
+    bs = BeautifulSoup(html, 'html.parser')  # Uses the html parser in the BS object
+    try:
+        print(bs.h1.get_text())
+        print(bs.find(id='mw-content-text').find_all('p')[0])
+        print(bs.find(id='ca-edit').find('span')
+              .find('a').attrs['href'])
+    except AttributeError:  # if something is missing it will print it
+        print('This page is missing something! Continuing.')
+    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                #We have encountered a new page
+                newPage = link.attrs['href']
+                print('-'*20)
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+getLinks('')
