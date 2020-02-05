f0388019a5db8e269520d2c944618925a3f5a4bf
Octavio <octavio.perezbravo@gmail.com>
2020-02-04 17:55:15 -0800

    Added a Wikipedia References saved and a Yellow Pages Scrapper

--- README.txt
+++ README.txt
@@ -3,3 +3,5 @@ The following libraries are necessary to run the crawlers:
 1- BeautifulSoup
 2- RE, for regular expressions
 3- Urllib to read the urls
+
+
--- WikiReferences.py
+++ WikiReferences.py
@@ -2,41 +2,49 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import csv
 import re
+
 linkList = []
 linkURL = []
 row = []
 
+def wikiscrapper(urllist):
+    for url in urllist:
+        print(url)
+        html = urlopen(url)
 
-def wikiscrapper(url):
+        bs = BeautifulSoup(html, 'html.parser')
+        container = bs.find('div', {'id': 'bodyContent'})
+        linkRe = re.compile('^(/wiki/)((?!:).)*$')
+        links = container.find_all('a', href=linkRe)
+        for link in links:
+            if 'href' in link.attrs:
 
-    x = 0
-    html = urlopen(url)
-    bs = BeautifulSoup(html, 'html.parser')
-    container = bs.find('div', {'id': 'bodyContent'})
-    linkRe = re.compile('^(/wiki/)((?!:).)*$')
-    links = container.find_all('a', href=linkRe)
-    for link in links:
-        if 'href' in link.attrs:
-            x = + x
+                column1 = link.attrs['title']
+                column2 = link.attrs['href']
+                row.append([column1.encode(encoding="UTF-8"), column2.encode(encoding="UTF-8")])
 
-            column1 = link.attrs['title']
-            column2 = link.attrs['href']
-            rowit = [column1, column2]
-            row.append(rowit)
-            #row = (column1, column2)
+                with open('wikires.csv', 'w', newline='') as csvfile:
+                    try:
+                        wikiwriter = csv.writer(csvfile)
+                        wikiwriter.writerows(row)
+                    except:
+                        print(column1+' Skipped because encoding')
 
-            with open('wikires.csv', 'w', newline='') as csvfile:
-                wikiwriter = csv.writer(csvfile)
-                wikiwriter.writerows(row)
-                #wikiwriter.writerow(linkURL)
+                linkList.append(link.attrs['title']) # regresa una lista que contiene los titulos de todos los articulos con referencias
+                linkURL.append(link.attrs['href'])
 
-            #q = len(link)
-            linkList.append(link.attrs['title']) # regresa una lista que contiene los titulos de todos los articulos con referencias
-            linkURL.append(link.attrs['href'])
+    return #linkList, linkURL
 
+def urlmaker(list):
+    urllist = []
+    for article in list:
+        article = article.replace(' ', '_')
+        url = ('https://en.wikipedia.org/wiki/' + article)
+        urllist.append(url)
+    return urllist
 
-    return linkList, linkURL
-
-wikiscrapper('https://en.wikipedia.org/wiki/Combination')
-print(linkList)
-#csvLoader(linkList, linkURL)
+articles = ('Combination', 'Computer scientist', 'Mathematician', 'Charles Kingsley', 'Evolution')
+urllist = urlmaker(articles)
+#wikiscrapper(urllist)
+wikiscrapper(urllist)
+#print(urllist)
