9bac1b558dcc4f45ac158f0b9c10b9f1371b614f
Octavio <octavio.perezbravo@gmail.com>
2020-02-06 12:20:06 -0800

    final code

--- YellowPages.py
+++ YellowPages.py
@@ -1,28 +1,31 @@
 from urllib.request import urlopen
-from urllib.error import  HTTPError
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import csv
+import pandas as pd
+import re
+import string
 
 linkList = []
 infoIterable = []
 urllist = []
 
 ''' Custom made function to scrap Yellow Pages website'''
-def scrapper(url):
 
+
+def scrapper(url):
     html = urlopen(url)
     bs = BeautifulSoup(html, 'html.parser')
     infoContainer = bs.find_all('div', {'class': 'v-card'})
 
     for info in infoContainer:
         try:
-
             name = info.find('a', {'class': 'business-name'}).get_text()
             categories = info.find('div', {'class': 'categories'}).get_text()
             try:
                 phone = info.find('div', {'class': 'phones phone primary'}).get_text()
             except:
-                phone = 'No phone'
+                phone = "No phone"
             try:
                 address = info.find('div', {'class': 'street-address'}).get_text()
             except:
@@ -30,42 +33,49 @@ def scrapper(url):
             try:
                 locality = info.find('div', {'class': 'locality'}).get_text()
             except:
-                locality = info.find('p', {'class': 'adr'}).get_text()
+                locality = url.replace('https://www.yellowpages.com/search?search_terms=dairy+farm&geo_location_terms=',
+                                       '')
+                locality = locality.replace('%2C+CA', '')
+                locality = locality.capitalize()
+
             row = (name, categories, phone, address, locality)
             infoIterable.append(row)
 
-            with open('results3.csv', 'w', newline='') as scrappy:
+            with open('results.csv', 'w', newline='') as scrappy:
                 writer = csv.writer(scrappy)
                 writer.writerows(infoIterable)
         except:
-            print('class Error')
+            print('Class Error')
             continue
 
-def urlmaker(citylist):
 
+def urlmaker(citylist):
     for city in citylist:
-        #print(city)
+        # print(city)
         city = city.replace(' ', '+')
         url = 'https://www.yellowpages.com/search?search_terms=dairy+farm&geo_location_terms=' + city + '%2C+CA'
         urllist.append(url)
     return urllist
+    # scrapper(url)
 
 
 ''' Start of the program'''
 
-cities =  ('alameda', 'alamo', 'albany',
-          'albion', 'alderpoint','arbuckle', 'alhambra', 'aliso viejo',
-          'alleghany', 'alpaugh', 'alpine', 'alta', 'altadena',
+cities = ('arbuckle', 'alameda', 'alamo', 'albany',
+          'albion', 'alderpoint', 'alhambra', 'aliso viejo', 'alleghany', 'alpaugh', 'alpine', 'alta', 'altadena',
           'angelus oaks')
 
 iter = urlmaker(cities)
 print(iter)
-
 for url in iter:
     try:
         scrapper(url)
         print(url)
 
     except HTTPError as e:
-        print(url, ' No farms founded')
+        print(url, ' No farms found')
         continue
+
+infoIterable = pd.DataFrame(infoIterable)
+tttessstttt = infoIterable.to_csv(r'C:\\Users\\IT\\Documents\\python\\testit.csv')
+
